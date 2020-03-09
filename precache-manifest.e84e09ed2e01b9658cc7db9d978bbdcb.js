9cb5baf1d7c2d7762f1a4c49182f3795f5bfbf1d
Carlos Massieu <cmassieu@outlook.com>
2020-03-09 14:00:32 -0600

    Prueba

---
+++ precache-manifest.e84e09ed2e01b9658cc7db9d978bbdcb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "528c49319b9bbea28632828f992946b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d5e3215a64e85eb805c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "835491f79365b503df57",
+    "url": "/static/js/2.8e2a0f70.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.8e2a0f70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d5e3215a64e85eb805c",
+    "url": "/static/js/main.2115894b.chunk.js"
+  },
+  {
+    "revision": "d4d633382d6be16077ce",
+    "url": "/static/js/runtime-main.328fad27.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
